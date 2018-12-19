550252415fbc3692e2db421388f28bac7e4950bf
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2018-12-19 11:00:06 -0600

    multi joint arm now

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+rm -rf docs
 parcel build index.html --out-dir docs --public-url https://dustinroepsch.github.io/2jointarm/
--- script.ts
+++ script.ts
@@ -26,79 +26,88 @@ function getMousePos(canvas, evt): Point {
     );
 }
 
-function distance( a: Point, b: Point): number {
-    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) **2);
+function distance(a: Point, b: Point): number {
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
 }
 
+function modulate(a: number[]): number[] {
+    let x: number[] = a.slice();
+    for (let i = 0; i < x.length; i++) {
+        x[i] += (Math.random() - .5) / 150;
+    }
+    return x;
+}
 
 
 class Arm {
-    private angle0: number = 0;
-    private angle1: number = 0;
-    private armLength: number = canvas.width / 2.5;
+    private armLength: number;
 
-    constructor(public positionX: number, public positionY: number) { }
+    public angles: number[] = [];
 
-    getP0(): Point {
-        return new Point(this.positionX + Math.cos(this.angle0) * this.armLength, this.positionY + Math.sin(this.angle0) * this.armLength);
+    constructor(public positionX: number, public positionY: number, numAngles: number) {
+        for (let i: number = 0; i < numAngles; i++) {
+            this.angles.push(0);
+        }
+        this.armLength = canvas.width / (numAngles + .5);
     }
 
-    getP1(): Point {
-        let p0 = this.getP0();
-        return new Point(p0.x + Math.cos(this.angle1) * this.armLength, p0.y + Math.sin(this.angle1) * this.armLength);
-    }
 
     render(): void {
+        let curX: number = this.positionX;
+        let curY: number = this.positionY;
+
+
+        for (let angle of this.angles) {
+            ctx.beginPath();
+            ctx.ellipse(curX, curY, 15, 15, 0, 0, Math.PI * 2);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(curX, curY);
+            curX += this.armLength * Math.cos(angle);
+            curY += this.armLength * Math.sin(angle);
+            ctx.lineTo(curX, curY);
+            ctx.stroke();
+        }
         ctx.beginPath();
-        ctx.ellipse(this.positionX, this.positionY, 10, 10, 0, 0, 2 * Math.PI);
+        ctx.ellipse(curX, curY, 15, 15, 0, 0, Math.PI * 2);
         ctx.stroke();
 
 
-        let p0: Point = this.getP0();
+    }
 
-        ctx.beginPath();
-        ctx.moveTo(this.positionX, this.positionY);
-        ctx.lineTo(p0.x, p0.y);
-        ctx.stroke();
+    private getFinalPoint(): Point {
+        let curX = this.positionX;
+        let curY = this.positionY;
 
-        ctx.beginPath();
-        ctx.ellipse(p0.x, p0.y, 10, 10, 0, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        let p1: Point = this.getP1();
-
-        ctx.beginPath();
-        ctx.moveTo(p0.x, p0.y);
-        ctx.lineTo(p1.x, p1.y);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.ellipse(p1.x, p1.y, 10, 10, 0, 0, 2 * Math.PI);
-        ctx.stroke();
+        for (let angle of this.angles) {
+            curX += this.armLength * Math.cos(angle);
+            curY += this.armLength * Math.sin(angle);
+        }
+        return new Point(curX, curY);
     }
 
     public moveTowards(pos: Point) {
-        let startingDistance: number = distance(pos, this.getP1());
-        let count = 0;
-
-        let startingA0 = this.angle0;
-        let startingA1 = this.angle1;
-        
-        while (count < 10 && distance(pos, this.getP1()) >= startingDistance) {
-            count++;
-            this.angle0 = startingA0 + (Math.random() - .5) / 2000;
-            this.angle1 = startingA1 + (Math.random() - .5) / 2000;
+        let startingDistance = distance(this.getFinalPoint(), pos);
+        if (startingDistance < 0.01) {
+            return;
         }
-        
-    }
+        let startingAngles: number[] = this.angles.slice();
 
+        for (let i: number = 0; i < 200; i++) {
+            this.angles = modulate(startingAngles);
+            if (distance(this.getFinalPoint(), pos) < startingDistance) {
+                return;
+            }
+        }
+    }
 }
 
 
-let arm: Arm = new Arm(canvas.width / 10, canvas.height / 10);
+let arm: Arm = new Arm(canvas.width / 10, canvas.height / 10, 4);
 let pos: Point = new Point(0, 0);
 canvas.addEventListener("mousemove", (evt) => {
-     pos = getMousePos(canvas, evt);
+    pos = getMousePos(canvas, evt);
 })
 
 
